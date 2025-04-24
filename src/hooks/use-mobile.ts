facaf312f1216d782a61186da21167f14f658eaf
Umair Ali <79967462+umair2602@users.noreply.github.com>
2025-04-24 16:58:50 +0500

    Update use-mobile.ts

--- src/hooks/use-mobile.ts
+++ src/hooks/use-mobile.ts
@@ -9,6 +9,7 @@ export function useIsMobile() {
     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
     const onChange = () => {
       setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+      
     }
     mql.addEventListener("change", onChange)
     setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
